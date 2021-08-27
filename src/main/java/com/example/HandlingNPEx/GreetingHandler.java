93e741b7f152c75c11a5ae24ed3988d7c15e3821
shhivaa00 <shhivaa00@gmail.com>
2021-08-28 02:54:47 +0530

    Reactive RESTful Web Service

--- src/main/java/com/example/HandlingNPEx/GreetingHandler.java
+++ src/main/java/com/example/HandlingNPEx/GreetingHandler.java
@@ -10,21 +10,53 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class GreetingHandler {
 
-    public Mono<ServerResponse> hellomono(ServerRequest request) {
+    public Mono<ServerResponse> helloMono(ServerRequest request) {
         Greeting greeting = new Greeting("Hello, Spring! This is mono");
-
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(greeting));
     }
 
-    public Mono<ServerResponse> helloflux(ServerRequest request) {
+    public Mono<ServerResponse> helloFlux(ServerRequest request) {
         Greeting greeting = new Greeting("Hello, Spring! This is mono");
-
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(greeting));
+    }
+
+    private Mono<String> sayHello(ServerRequest request) {
+//        Greeting greeting = new Greeting("Hello, Spring! This is mono");
+
+//        Optional<String> queryparam = Optional.of(request.queryParam("name").get());
+//        return Mono.just("Hello, " + queryparam.orElseGet(null));
+
+        if(!request.queryParam("name").isPresent()){
+            return Mono.just("Failed Name");
+        }
+
+        return Mono.justOrEmpty("Hello, " + request.queryParam("name").orElseGet(null));
+        //...
+    }
+
+    public Mono<ServerResponse> handleOnReturn(ServerRequest request) {
+        return sayHello(request)
+                .onErrorReturn("Hey! onErrorReturn Handle: Sorry there is an Error ")
+                .flatMap(s -> ServerResponse.ok()
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .bodyValue(s));
+    }
+
+    public Mono<ServerResponse> handleOnResume(ServerRequest request) {
+        return sayHello(request)
+                .flatMap(s -> ServerResponse.ok()
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .bodyValue(s))
+                .onErrorResume(e -> Mono.just("Hey! onErrorResume Handle: Sorry there is an Error " + e.getMessage())
+                        .flatMap(s -> ServerResponse.ok()
+                                .contentType(MediaType.TEXT_PLAIN)
+                                .bodyValue(s)));
     }
 }
--- src/main/java/com/example/HandlingNPEx/GreetingRouter.java
+++ src/main/java/com/example/HandlingNPEx/GreetingRouter.java
@@ -3,6 +3,7 @@ package com.example.HandlingNPEx;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -14,16 +15,33 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 public class GreetingRouter {
 
     @Bean
-    public RouterFunction<ServerResponse> routemono(GreetingHandler greetingHandler) {
-
+    public RouterFunction<ServerResponse> routeMono(GreetingHandler greetingHandler) {
         return RouterFunctions
-                .route(GET("/mono").and(accept(MediaType.APPLICATION_JSON)), greetingHandler::hellomono);
+                .route(GET("/mono")
+                        .and(accept(MediaType.APPLICATION_JSON)), greetingHandler::helloMono);
     }
 
     @Bean
-    public RouterFunction<ServerResponse> routeflux(GreetingHandler greetingHandler) {
-
+    public RouterFunction<ServerResponse> routeFlux(GreetingHandler greetingHandler) {
         return RouterFunctions
-                .route(GET("/flux").and(accept(MediaType.APPLICATION_JSON)), greetingHandler::helloflux);
+                .route(GET("/flux")
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                        greetingHandler::helloFlux);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routeReturn(GreetingHandler greetingHandler) {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/onReturn")
+                        .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
+                greetingHandler::handleOnReturn);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routeResume(GreetingHandler greetingHandler) {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/onResume")
+                        .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
+                greetingHandler::handleOnResume);
     }
 }
